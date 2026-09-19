92f014481927916da1c39b4ab94b999f369ea0f9
Neskazu9 <kewr777@mail.ru>
2026-09-19 18:58:08 +0300

    Add pytest fixture for Selenium WebDriver

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://b2c.passport.rt.ru/")
+
+    yield driver
+
+    driver.quit()
